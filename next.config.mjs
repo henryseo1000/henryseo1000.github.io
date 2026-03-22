d50cb681605353b120207a0a1063e9c23ada4e24
서호준 <henryseo1000@gmail.com>
2026-03-22 20:59:53 +0900

    tailwind css 미적용 문제 해결 - assetPrefix 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/henryseo1000.github.io'
+    basePath: '/henryseo1000.github.io',
+    assetPrefix : process.env.NODE_ENV === "production" ? "https://henryseo1000.github.io" : ""
 };
 
 export default nextConfig;
